845f4115f64d7f13e23363efb40fd531df19efcf
Bridgett Coyle <bacoyle0409@gmail.com>
2019-04-13 11:47:09 -0600

    Add start of project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './Styles/normalize.scss';
+import './Styles/index.scss';
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App/App.js';
+
+ReactDom.render(<App />,
+document.getElementById('root'));
